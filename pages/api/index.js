9c18a2fad858639c7de8bb294558cfd079438793
Madusha Prasanjith <hi@madusha.me>
2023-01-13 15:31:17 +0100

    feat: Add sentry proxy.

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,20 +1,57 @@
-const axios = require('axios');
+// Change host appropriately if you run your own Sentry instance.
+const sentryHost = "sentry.io";
 
-export default function handler(req, res, next) {
-  async function getURI(url) {
-    try {
-      const response = await axios.get(url);
-      console.log(response)
-      if (response.status !== 200) {
-        return res.status(response.status).json({ type: 'error', message: response.statusText });
-      } else {
-      res.json(response.data);
-      }
-    } catch (error) {
-      console.log(error)
-      return res.status(500).json({ type: 'error', message: error.message });
-    }
+// Set knownProjectIds to an array with your Sentry project IDs which you
+// want to accept through this proxy.
+const knownProjectIds = ["4504496775561216"];
+
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern; but there might not be origin (for instance call from browser)
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,POST");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
   }
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  getURI(req.body['my-url'])
+  return await fn(req, res);
+};
+
+async function handler(req, res) {
+  try {
+    const envelope = req.body;
+    const pieces = envelope.split("\n");
+    const header = JSON.parse(pieces[0]);
+    // DSNs are of the form `https://<key>@o<orgId>.ingest.sentry.io/<projectId>`
+    const { host, pathname } = new URL(header.dsn);
+    // Remove leading slash
+    const projectId = pathname.substring(1);
+
+    if (host !== sentryHost) {
+      throw new Error(`invalid host: ${host}`);
+    }
+
+    if (!knownProjectIds.includes(projectId)) {
+      throw new Error(`invalid project id: ${projectId}`);
+    }
+
+    const sentryIngestURL = `https://${sentryHost}/api/${projectId}/envelope/`;
+    const sentryResponse = await fetch(sentryIngestURL, {
+      method: "POST",
+      body: envelope,
+    });
+
+    // Relay response from Sentry servers to front end
+    sentryResponse.headers.forEach(([key, value]) => res.setHeader(key, value));
+    res.status(sentryResponse.status).send(sentryResponse.body);
+  } catch (e) {
+    return res.status(400).json({ status: "invalid request" });
+  }
 }
+
+export default allowCors(handler);
